0a9ad088eb3bba646a522e3ee7d42e4f95f3bff0
FANNY-KOT <borisenkomaksim10@gmail.com>
2026-01-29 16:26:51 +0200

    Add pr/5

--- js/app/app.js
+++ js/app/app.js
@@ -1,10 +1,9 @@
 import { router } from './router.js'
 import { header } from './widgets/header.js'
-// import { search } from './widgets/search.js'
 import { popup } from './widgets/popup.js'
 import { msg } from './widgets/msg.js'
 import { toogle } from './widgets/toogle.js'
-// import { img } from './widgets/img.js'
+import { img } from './widgets/img.js'
 
 document.addEventListener('DOMContentLoaded', function () {
   const main = {
@@ -114,12 +113,11 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 
   const app = Vue.createApp(main)
+    .component('Image', img)
     .component('Header', header)
     .component('popup', popup)
-    // .component('search', search)
     .component('msg', msg)
     .component('toogle', toogle)
-    // .component('Image', img)
     .use(router)
 
   app.mount('#content')
--- js/app/router.js
+++ js/app/router.js
@@ -1,6 +1,6 @@
 import { login } from './pages/login.js'
 import { campaigns } from './pages/campaigns.js'
-// import { campaign } from './pages/campaign.js'
+import { campaign } from './pages/campaign.js'
 // import { users } from './pages/users.js'
 // import { user } from './pages/user.js'
 // import { ads } from './pages/ads.js'
@@ -12,8 +12,8 @@ export const router = VueRouter.createRouter({
   history: VueRouter.createWebHashHistory(),
   routes: [
     { path: '/', name: 'Sign in', component: login },
-    { path: '/campaigns', name: 'Campaigns', component: campaigns }
-    // { path: '/campaign/:id', name: 'Campaign', component: campaign },
+    { path: '/campaigns', name: 'Campaigns', component: campaigns },
+    { path: '/campaign/:id', name: 'Campaign', component: campaign }
     // { path: '/users', name: 'Users', component: users },
     // { path: '/user/:id', name: 'User', component: user },
     // { path: '/ads', name: 'Ads', component: ads },
